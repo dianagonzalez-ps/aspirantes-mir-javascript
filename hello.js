325b8f2be9d09f8b68bfe46d739aaa04ef850e80
dianagonzalez-ps <dianagarzon.ps@gmail.com>
2023-05-12 00:56:54 -0500

    solución a los ejercicios de js

---
+++ hello.js
@@ -0,0 +1,9 @@
+function hello(name) {
+
+    return "Hola " + name;
+}
+
+console.log(hello ("Pedro"));
+console.log(hello ("Juan"));
+console.log(hello (""));
+
---
+++ suma.js
@@ -0,0 +1,5 @@
+function suma (num) {
+    return (num * (num + 1)/ 2);
+}
+
+console.log(suma(4));
